2e42ab6f64bf3ac61b15c6362f3394331071ba4c
diegod3v <diegod3v@gmail.com>
2020-07-16 16:08:49 -0500

    add apollo config

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import '../styles/index.css'
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '../lib/apolloClient'
+
+function MyApp({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps.initialApolloState)
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
+}
+
+export default MyApp
